afdfdc173c6cce07b559f72a562e79fa0d548d4e
42401842-netizen <42401842@uaz.edu.mx>
2026-04-14 20:40:46 +0100

    Funcionalidad antes del botón editar

---
+++ src/main/java/teoria/labuivjfxnomina/App.java
@@ -0,0 +1,56 @@
+package teoria.labuivjfxnomina;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("nomina-view"), 872, 629);
+        stage.setScene(scene);
+        stage.setTitle("Nómina");
+        stage.setOnCloseRequest(event -> {
+            event.consume(); // bloquea el cierre automático
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmar salida");
+            alert.setHeaderText("¿Estás seguro que deseas salir?");
+            alert.setContentText("Presiona Aceptar para salir o Cancelar para permanecer.");
+
+            Optional<ButtonType> resultado = alert.showAndWait();
+
+            if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
+                System.exit(0);
+            }
+        });
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/teoria/labuivjfxnomina/NominaController.java
@@ -0,0 +1,226 @@
+package teoria.labuivjfxnomina;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class NominaController implements Initializable {
+
+    @FXML
+    private TextField txtMatricula, txtNombre, txtApellidos, txtHorasTrabajadas, txtBuscar;
+
+    @FXML
+    private TableView<Empleado> tblListaDeEmpleados;
+    
+    @FXML
+    private TableColumn<Empleado, String> colMatricula;
+
+    @FXML
+    private TableColumn<Empleado, String> colNombre;
+
+    @FXML
+    private TableColumn<Empleado, String> colApellidos;
+
+    @FXML
+    private TableColumn<Empleado, String> colPuesto;
+
+    @FXML
+    private TableColumn<Empleado, String> colHorasTrabajadas;
+
+    @FXML
+    private TableColumn<Empleado, String> colPagoXHora;
+
+    @FXML
+    private TableColumn<Empleado, String> colSalarioNeto;
+
+    /**
+     * Lista observable que almacenará los empleados. El TableView mostrará
+     * automáticamente lo que se agregue aquí
+     */
+    private ObservableList<Empleado> listaEmpleados;
+    
+     /**
+     * Lista auxiliar para mostrar resultados filtrados
+     */
+    private ObservableList<Empleado> listaFiltrada = FXCollections.observableArrayList();
+
+    @FXML
+    private ComboBox cboPuesto;
+
+    /**
+     * Este método se ejecuta automáticamente cuando se carga el archivo FXML
+     * Carga los items del combobox
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Elementos o ítems que se agregarán al combobox
+        cboPuesto.getItems().addAll("Administrador", "Gerente", "Empleado");
+
+        // establece el valor predeterminado
+        cboPuesto.getSelectionModel().select("Administrador");
+
+        //Relacionar cada columna con el atributo correspondiente de la clase Empleado
+        colMatricula.setCellValueFactory(new PropertyValueFactory<>("matricula"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
+        colPuesto.setCellValueFactory(new PropertyValueFactory<>("puesto"));
+        colHorasTrabajadas.setCellValueFactory(new PropertyValueFactory<>("horasTrabajadas"));
+        colPagoXHora.setCellValueFactory(new PropertyValueFactory<>("pagoXHoras"));
+        colSalarioNeto.setCellValueFactory(new PropertyValueFactory<>("salarioNeto"));
+
+        // Crear la lista observable
+        listaEmpleados = FXCollections.observableArrayList();
+
+        // Asignar la lista al TableView
+        tblListaDeEmpleados.setItems(listaEmpleados);
+    }
+
+    @FXML
+    protected void onAgregarEmpleadoButtonClick() {
+        // obtenemos los datos capturados
+        String matricula = txtMatricula.getText();
+        String nombre = txtNombre.getText();
+        String apellidos = txtApellidos.getText();
+        String puesto = (String) cboPuesto.getValue();
+        double horasTrabajadas = Double.parseDouble(txtHorasTrabajadas.getText());
+        double pagoXHora = 800.0;
+
+        // Creamos el objeto Empleado
+        Empleado empleado = new Empleado(matricula, nombre, apellidos, puesto, horasTrabajadas, pagoXHora);
+
+        // cuando se le paga por hora.
+        listaEmpleados.add(empleado);
+        //llama para limpiar el formulario
+        limpiarFormulario();
+    }
+    
+    /**
+     * Limpia el formulario de captura de datos
+     */
+    @FXML
+    protected void limpiarFormulario() {
+        txtMatricula.setText("");
+        txtNombre.setText("");
+        txtApellidos.setText("");
+
+        // establece el valor predeterminado de la lista desplegable
+        cboPuesto.getSelectionModel().select("Administrador");
+
+        txtHorasTrabajadas.setText("");
+
+        txtMatricula.requestFocus();
+    }
+    
+    /**
+     * Elimina un empleado seleccionado
+     */
+    @FXML
+    protected void onEliminarEmpleadoButtonClick() {
+        // Obtenemos el empleado actualmente seleccionado en el TableView
+        Empleado empleadoSeleccionado = tblListaDeEmpleados.getSelectionModel().getSelectedItem();
+
+        // Validamos que realmente se seleccionó algo
+        if (empleadoSeleccionado == null) {
+            // importa el paquete correspondiente para que no te marque error
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Sin selección");
+            alert.setHeaderText(null);
+            alert.setContentText("Debes seleccionar un empleado para poderlo eliminar");
+            alert.showAndWait();
+            return;
+        }
+
+        // Mostrar confirmación antes de eliminar
+        Alert confirmacion = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmacion.setTitle("Confirmar Eliminación");
+        confirmacion.setHeaderText(null);
+        confirmacion.setContentText("Deseas eliminar el empleado: "
+                + empleadoSeleccionado.getNombre() + " "
+                + empleadoSeleccionado.getApellidos());
+
+        // importa los paquetes correspondientes
+        Optional<ButtonType> resultado = confirmacion.showAndWait();
+
+        // Si el usuario confirma, se elimina de la lista observable
+        if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
+            listaEmpleados.remove(empleadoSeleccionado);
+
+            Alert informacion = new Alert(Alert.AlertType.INFORMATION);
+            informacion.setTitle("Eliminación exitosa");
+            informacion.setHeaderText(null);
+            informacion.setContentText("El empleado fue eliminado correctamente.");
+            informacion.showAndWait();
+        }
+    } // fin del método eliminar
+    
+    /**
+     * Permite buscar un empleado por matrícula o por
+     * nombre
+     */
+    @FXML
+    protected void onBuscarEmpleadoButtonClick() {
+        // obtener el texto escrito por el usuario
+        String textoBusqueda = txtBuscar.getText().trim().toLowerCase();
+
+        // Si el campo está vacío, mostrar todos los empleados
+        if (textoBusqueda.isEmpty()) {
+            tblListaDeEmpleados.setItems(listaEmpleados);
+            return;
+        }
+
+        // Limpiar resultados anteriores
+        listaFiltrada.clear();
+
+        // Recorrer la lista maestra y buscar coincidencias
+        for (Empleado empleado : listaEmpleados) {
+            boolean coincideMatricula = empleado.getMatricula().toLowerCase().contains(textoBusqueda);
+            boolean coincideNombre = empleado.getNombre().toLowerCase().contains(textoBusqueda);
+
+            if (coincideMatricula || coincideNombre) {
+                listaFiltrada.add(empleado);
+            }
+        }
+
+        // actualizar setItems (listaFiltrada)
+        tblListaDeEmpleados.setItems(listaFiltrada);
+    }
+    
+    /**
+     * Muestra la lista de todos los empleados
+     */
+    @FXML
+    protected void onMostrarTodosButtonClick() {
+        txtBuscar.clear();
+        tblListaDeEmpleados.setItems(listaEmpleados);
+    }
+    
+    @FXML
+    protected void onSalirButtonClick() {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmar salida");
+        alert.setHeaderText("¿Estás seguro que deseas salir?");
+        alert.setContentText("Presiona Aceptar para salir o Cancelar para permanecer.");
+
+        Optional<ButtonType> resultado = alert.showAndWait();
+
+        if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
+            System.exit(0);
+        }
+    }
+}
